acfe9dc3ce171e0e3ce367c1854201016ea691b2
gurkandoner7 <150553508+gurkandoner7@users.noreply.github.com>
2023-11-12 13:37:31 +0300

    added splash screen, custom views and extensions

--- app/src/main/java/com/portal/asciiproject/ui/main/MainActivity.kt
+++ app/src/main/java/com/portal/asciiproject/ui/main/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.portal.asciiproject.ui.main
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
-import androidx.navigation.NavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import com.portal.asciiproject.compose.viewInflateBinding
 import com.portal.asciiproject.R
+import com.portal.asciiproject.compose.viewInflateBinding
 import com.portal.asciiproject.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,10 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-
         navController.navigate(R.id.splashFragment)
     }
 
